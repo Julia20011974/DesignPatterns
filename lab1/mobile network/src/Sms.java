024f2ca03831cb8656b43a0c6a45f1d78b348493
Julia20011974 <julia19742001@mail.ru>
2021-02-25 03:04:13 +0300

    Create class Sms

---
+++ lab1/mobile network/src/Sms.java
@@ -0,0 +1,13 @@
+
+public class Sms extends Connection {
+
+	private String recvPhone;
+
+	public void setRecvPhone(String phone) {
+		this.recvPhone = phone;
+	}
+	
+	public String getRecvPhone() {
+		return recvPhone;
+	}
+}
